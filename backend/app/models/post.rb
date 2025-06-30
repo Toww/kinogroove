d0aa62cdffcd0dbb110a6ba26ffdd27eddefa3ac
Toww <25932452+Toww@users.noreply.github.com>
2025-06-30 17:22:52 +0200

    feat: handling hashtag creation from post

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,4 +1,22 @@
 class Post < ApplicationRecord
   # Associations
+  has_many :post_hash_tags
+  has_many :hash_tags, through: :post_hash_tags
+
+  # Active Record
   has_one_attached :song
+
+  # Callbacks
+  after_commit :create_hash_tags, on: :create
+
+  def create_hash_tags
+    # This regex gets the word without the "#", and works with accents.
+    # The scan result is flattened ti fix array of arrays caused by
+    # the  single capture groups in the regex.
+    tags = self.body.scan(/(?<!\S)#([\p{L}\p{M}0-9_-]+)/).flatten
+
+    tags.each do |tag|
+      self.hash_tags.create(name: tag)
+    end
+  end
 end
