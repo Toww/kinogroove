644d80fe8ca53c6f4d419fb13f787cd7b7d2377b
Toww <25932452+Toww@users.noreply.github.com>
2025-08-08 17:34:32 +0200

    perf: replacing kaminari for pagy

--- frontend/src/components/posts/Pagination.jsx
+++ frontend/src/components/posts/Pagination.jsx
@@ -5,14 +5,13 @@ import { PostsContext } from "../../contexts/PostsContext";
 
 const Pagination = () => {
   // Context
-  const { error, loading, totalPosts, postsPerPage, fetchPosts } =
+  const { error, loading, fetchPosts, pagination, pagesCount } =
     useContext(PostsContext);
 
   // Hooks
   const [searchParams, setSearchParams] = useSearchParams();
 
   // Variables
-  const totalPages = Math.ceil(totalPosts / postsPerPage);
   const currentPage = Number(searchParams.get("page")) || 1;
 
   // Handlers
@@ -22,51 +21,19 @@ const Pagination = () => {
   };
 
   // Getters
-  const getRangeArray = (start, end) => {
-    const rangeArray = [];
-    for (let i = start; i <= end; i++) {
-      rangeArray.push(i);
-    }
-    return rangeArray;
-  };
-
   const getPaginationButtons = () => {
-    // Variables
-    const maxInlinePages = 8;
-    const sideNumbersOnEllipsis = 4;
-
-    // If there is less than n pages, show all buttons, ex :
-    // <- 1 2 3 4 5 6 7 8 9 10 ->
-    if (totalPages <= maxInlinePages) {
-      return getRangeArray(1, totalPages);
-    }
-
-    // Else, we should use ellipsises, depending on the current page
-    // First pages :
-    // <- 1 2 3 4 5 6 ... 99 ->
-    if (currentPage <= sideNumbersOnEllipsis - 1) {
-      return [...getRangeArray(1, sideNumbersOnEllipsis), "...", totalPages];
-    }
-
-    // Last pages
-    // <- 1 ... 95 96 97 98 99 ->
-    if (currentPage >= totalPages - (sideNumbersOnEllipsis - 1)) {
-      return [
-        1,
-        "...",
-        ...getRangeArray(totalPages - sideNumbersOnEllipsis, totalPages),
-      ];
-    }
-
-    // Middle pages
-    // <- 1 ... 49 50 51  ... 99 ->
-    return [
-      1,
-      "...",
-      ...getRangeArray(currentPage - 1, currentPage + 1),
-      "...",
-      totalPages,
-    ];
+    return (
+      pagination &&
+      pagination.map((pageNum, index) => (
+        <PaginationButton
+          key={`page-btn-${index}`}
+          currentPage={currentPage}
+          onClick={() => pageNum !== "gap" && handlePageChange(pageNum)}
+        >
+          {pageNum !== "gap" ? `${pageNum}` : "..."}
+        </PaginationButton>
+      ))
+    );
   };
 
   // Conditions
@@ -86,18 +53,10 @@ const Pagination = () => {
       )}
 
       {/* Page numbers */}
-      {getPaginationButtons().map((pageNum, index) => (
-        <PaginationButton
-          currentPage={currentPage}
-          key={`page-btn-${index}`}
-          onClick={() => handlePageChange(pageNum)}
-        >
-          {pageNum}
-        </PaginationButton>
-      ))}
+      {getPaginationButtons()}
 
       {/* Next button */}
-      {currentPage !== totalPages && (
+      {currentPage !== pagesCount && (
         <PaginationButton
           currentPage={currentPage}
           onClick={() => handlePageChange(currentPage + 1)}
